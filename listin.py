b561fa555fc4f920fcd9b0daec5a6b69d4145fde
ism49186163 <ism49186163@a02.informatica.escoladeltreball.org>
2018-04-16 10:07:48 +0200

    Version 1.0

---
+++ listin.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+dicTel={}
+def funcionTel():
+    nom=input("Dime nombre a introducir: ")
+    telefon=input("Dime telefono a introducir: ")
+    pregunta=input("Quiere introducir más contactos?")
+    if nom in dicTel:
+            print ("Error, el nombre está repetido")
+    else:
+        dicTel[nom]=telefon
+    
+    while (pregunta=="si" or pregunta=="Si" or pregunta=="SI"):
+        nom=input("Dime nombre a introducir: ")
+        telefon=input("Dime telefono a introducir: ")
+        if nom in dicTel:
+            print ("Error, el nombre está repetido")
+        else:
+            dicTel[nom]=telefon
+        pregunta=input("Quiere introducir más contactos?")
+
+funcionTel()
+print ("Tu lista de contactos es: ",dicTel)
